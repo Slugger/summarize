d2393aa8453d13d0d29634d4a9e90594f1c6ad7a
Derek Battams <derek@battams.ca>
2015-10-08 13:27:42 -0400

    Fix typo

--- src/main/groovy/com/github/slugger/summarize/DataStore.groovy
+++ src/main/groovy/com/github/slugger/summarize/DataStore.groovy
@@ -185,7 +185,7 @@ class DataStore {
 			}
 			tasks << getCompletedTaskById(it.ct_id)
 		}
-		builds.sort { -1L * b.value.min { it.start }.start.time	}
+		builds.sort { -1L * it.value.min { it.start }.start.time }
 	}
 	
 	CompletedTask getCompletedTaskById(long id) {
